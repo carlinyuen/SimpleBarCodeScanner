6618f9b4d0490d387883af4d6bbbefe6d4317310
Carlin <email.me@carlinyuen.com>
2013-12-21 11:39:58 -0800

    fleshing out iOS 7 implementation

--- SimpleBarCodeScanner/SimpleBarCodeScanner.h
+++ SimpleBarCodeScanner/SimpleBarCodeScanner.h
@@ -8,6 +8,38 @@
 
 #import <Foundation/Foundation.h>
 
+@class SimpleBarCodeScanner;
+@protocol SimpleBarCodeScannerDelegate <NSObject>
+
+	@required
+	- (void)scanner:(SimpleBarCodeScanner *)scanner scannedCode:(NSString *)code;
+
+@end
+
 @interface SimpleBarCodeScanner : NSObject
+
+    /** Delegate */
+    @property (nonatomic, weak) id<SimpleBarCodeScannerDelegate> delegate;
+
+    /** Last scanned code */
+    @property (nonatomic, copy) NSString *lastCode;
+
+    /** Types of codes to scan, defaults to all */
+    @property (nonatomic, strong) NSArray *codeTypes;
+
+    /** Color of highlight frame, defaults to red */
+    @property (nonatomic, strong) UIColor *highlightColor;
+    
+    /** Width of border of highlight frame, default = 2.0 */
+    @property (nonatomic, assign) CGFloat highlightWidth; 
+
+    /** Initialize scanner with view to be used for displaying the scanning */
+    - (id)initWithView:(UIView *)view;
+    
+    /** Start capture session */
+    - (void)start;
+    
+    /** Stop capture session */
+    - (void)stop;
 
 @end
--- SimpleBarCodeScanner/SimpleBarCodeScanner.m
+++ SimpleBarCodeScanner/SimpleBarCodeScanner.m
@@ -8,8 +8,160 @@
 
 #import "SimpleBarCodeScanner.h"
 
+#import <AVFoundation/AVFoundation.h>
+#import <QuartzCore/QuartzCore.h>
+
 #import "ZBarSDK.h"
 
+@interface SimpleBarCodeScanner() <
+    AVCaptureMetadataOutputObjectsDelegate
+>
+    
+    /** View to display scanning in */
+    @property (nonatomic, strong) UIView *view;
+    @property (nonatomic, strong) UIView *highlightView; 
+
+    // iOS 7 Scanning with AVCapture
+    @property (nonatomic, strong) AVCaptureSession *session;
+    @property (nonatomic, strong) AVCaptureDevice *device;
+    @property (nonatomic, strong) AVCaptureDeviceInput *input; 
+    @property (nonatomic, strong) AVCaptureMetadataOutput *output;  
+    @property (nonatomic, strong) AVCaptureVideoPreviewLayer *preview;   
+    
+    // ZBarSDK Scanning
+
+@end
+
 @implementation SimpleBarCodeScanner
+
+
+/** Initialize scanner with view to be used for displaying the scanning */
+- (id)initWithView:(UIView *)view
+{
+    self = [super init];
+    if (self)
+    {
+        _lastCode = nil;
+        _codeTypes = @[
+            AVMetadataObjectTypeAztecCode, 
+            AVMetadataObjectTypeCode128Code, 
+            AVMetadataObjectTypeCode39Code, 
+            AVMetadataObjectTypeCode39Mod43Code,
+            AVMetadataObjectTypeCode93Code, 
+            AVMetadataObjectTypeEAN13Code, 
+            AVMetadataObjectTypeEAN8Code,  
+            AVMetadataObjectTypePDF417Code, 
+            AVMetadataObjectTypeQRCode, 
+            AVMetadataObjectTypeUPCECode,   
+        ];
+    
+        _view = view;
+        _highlightView = [UIView new];
+        _highlightView.backgroundColor = [UIColor clearColor];
+        _highlightView.layer.borderColor = [[UIColor colorWithRed:1.0 green:0 blue:0 alpha:0.8] CGColor];
+        _highlightView.layer.borderWidth = 2.0; 
+        _highlightColor = nil;
+        _highlightWidth = 0;
+        
+        _session = [[AVCaptureSession alloc] init];
+        _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        
+        NSError *error = nil;
+        _input = [AVCaptureDeviceInput deviceInputWithDevice:_device error:&error];
+        if (_input) {
+            [_session addInput:_input];
+        } else {
+            NSLog(@"Error Initializing Bar Code Scanner: %@", error);
+        }
+
+        _output = [[AVCaptureMetadataOutput alloc] init];
+        [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+        [_session addOutput:_output];
+
+        _output.metadataObjectTypes = [_output availableMetadataObjectTypes];
+
+        _preview = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+        _preview.frame = _view.bounds;
+        _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        [_view.layer addSublayer:_preview];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [_session stopRunning];
+    _session = nil;
+    _input = nil;
+    _output = nil;
+    [_preview removeFromSuperlayer];
+    _preview = nil;
+}
+
+/** Start capture session */
+- (void)start
+{
+    [self.session startRunning];
+}
+
+/** Stop capture session */
+- (void)stop
+{
+    [self.session stopRunning];
+}
+
+
+#pragma mark - AVCaptureMetadataOutputObjectsDelegate
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    CGRect highlightViewRect = CGRectZero;
+    AVMetadataMachineReadableCodeObject *barCodeObject;
+    NSString *detectionString = nil;
+    bool foundCode = false;
+
+    for (AVMetadataObject *metadata in metadataObjects) {
+        if ([self.codeTypes containsObject:metadata.type])
+        {
+            barCodeObject = (AVMetadataMachineReadableCodeObject *)
+                [self.preview transformedMetadataObjectForMetadataObject:
+                    (AVMetadataMachineReadableCodeObject *)metadata];
+            
+            highlightViewRect = barCodeObject.bounds;
+            detectionString = 
+                [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
+                
+            if (detectionString != nil) {
+                foundCode = true;
+                break;
+            }
+        }
+    }
+
+    // Update color / width of highlight if changed
+    if (self.highlightWidth > 0) {
+        self.highlightView.layer.borderWidth = self.highlightWidth;
+    }
+    if (self.highlightColor) {
+        self.highlightView.layer.borderColor = [self.highlightColor CGColor];
+    }
+       
+    // Animate highlightview
+    [UIView animateWithDuration:0.2 delay:0 
+        options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut 
+        animations:^{
+            self.highlightView.frame = highlightViewRect; 
+            self.highlightView.alpha = (foundCode) ? 1 : 0;
+        } 
+        completion:^(BOOL finished) {
+            if (foundCode) 
+            {
+                self.lastCode = detectionString; // Update last code scanned   
+                if (self.delegate) {
+                    [self.delegate scanner:self scannedCode:self.lastCode];
+                }
+            } 
+        }];
+}
 
 @end
